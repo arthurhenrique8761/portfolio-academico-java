817ae98cfaf25b05133610285ef2ad558632dbae
Arthur Henrique <arthurhenrique1678@gmail.com>
2026-09-17 23:14:03 -0300

    Update main and services package

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,64 @@
 import model.Projeto;
+import service.ProjetoService;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("===========================");
         System.out.println("    SISTEMA DE PROJETOS");
         System.out.println("===========================");
-        System.out.println("Bem-vindo ao sistema!");
+        System.out.println("   Bem-vindo ao sistema!");
         System.out.println("\nProjeto: Portifólio Acadêmico");
         System.out.println("Desenvolvido em Java");
-        System.out.println("Versão: 1.0");
+        System.out.println("Versão: 1.0\n");
 
-        Projeto projeto1 = new Projeto(
-            1,
-            "Sistema Acadêmico",
-            "Software para gerenciamento acadêmico",
-            "Software",
-            "Em desenvolvimento"
-        );
+        ProjetoService projetoService = new ProjetoService();
 
-        Projeto projeto2 = new Projeto(
-            2,
-            "Site Institucional",
-            "Web site de uma instituição",
-            "Web",
-            "Concluído"
-        );
-        
-        projeto1.exibirDados();
-        projeto2.exibirDados();
+        Projeto p1 = new Projeto(1, "Painel de Atendimento", "Painel para atendimento médico", "Web", "Concluído");
+        Projeto p2 = new Projeto(2, "Portfolio Academico", "Portfolio Academico em Java", "Software", "Em desenvolvimento");
+        Projeto p3 = new Projeto(3, "Aplicativo Mobile", "Aplicativo para prestação de serviço", "Mobile", "Planejado");
+        Projeto p4 = new Projeto(4, "Portal de Eventos", "Portal para eventos", "Web", "Em desenvolvimento");
+        Projeto p5 = new Projeto(5, "Sistema financeiro", "Sistema financeiro em Java", "Software", "Concluído");
 
+        projetoService.adicionar(p1);
+        projetoService.adicionar(p2);
+        projetoService.adicionar(p3);
+        projetoService.adicionar(p4);
+        projetoService.adicionar(p5);
+
+        System.out.println();
+
+        System.out.println("TOTAL DE PROJETOS: " + projetoService.listar().size() + "\n");
+
+        System.out.println("LISTA DE PROJETOS");
+        for (Projeto projeto : projetoService.listar()) {
+            projeto.exibirDados();
+        }
+
+        System.out.println("\nBUSCA PELO ID 3");
+        Projeto encontrado = projetoService.buscaPorId(3);
+        if (encontrado != null) {
+            System.out.println("Projeto encontrado");
+            encontrado.exibirDados();
+        }
+        else {
+            System.out.println("Projeto não encontrado");
+        }
+
+        System.out.println("\nPROJETOS WEB");
+        for (Projeto projeto : projetoService.buscarPorCategoria("Web")) {
+            projeto.exibirDados();
+        }
+
+        System.out.println("\nPROJETOS CONCLUÍDOS");
+        for (Projeto projeto : projetoService.buscarPorStatus("concluído")) {
+            projeto.exibirDados();
+        }
+
+        System.out.println("\nCADASTRO DE PRODUTO COM ID REPETIDO (1)");
+        Projeto p6 = new Projeto(1, "Emulador de Master System", "Emulador desktop de Master System", "Software", "Em desenvolvimento");
+        projetoService.adicionar(p6);
+
+        System.out.println("\nREMOVER PROJETO 4");
+        projetoService.removerPorId(4);
     }
 }
--- src/service/ProjetoService.java
+++ src/service/ProjetoService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import model.Projeto;
 
+// regras de negócio
 public class ProjetoService {
     private List<Projeto> projetos;
 
@@ -12,20 +13,29 @@ public class ProjetoService {
         projetos = new ArrayList<>();
     }
 
-    public void adicionar(Projeto projeto) {
-        projetos.add(projeto);
+    public boolean adicionar(Projeto projeto) {
+        Projeto existente = buscaPorId(projeto.getId());
+        if (projeto.getNome().isBlank()) {
+            return false;
+        }
+        if (existente != null) {
+            System.out.println("Erro: Projeto já existente");
+            return false;
+        }
+        else {
+            projetos.add(projeto);
+            System.out.println("Projeto " + projeto.getId() + " adicionado com sucesso");
+            return true;
+        }
     }
 
-    public void listar() { // TO DO: voltar lista de projeto
-        projetos.stream()
-        .forEach(n -> System.out.println(n));
+    public List<Projeto> listar() { // TO DO: retornar uma lista
+        List<Projeto> resultado = projetos.stream().toList();
+        return resultado;
     }
 
-    public void totalDeProjetos() {
-        System.out.println("Total de projetos: " + projetos.size());
-    }
-
-    public Projeto buscarPorId(int id) {
+    // buscas
+    public Projeto buscaPorId(int id) {
         for (Projeto projeto : projetos) {
             if (projeto.getId() == id)
                 return projeto;
@@ -34,13 +44,29 @@ public class ProjetoService {
     }
 
     public List<Projeto> buscarPorCategoria(String categoria) {
-        List<Projeto> resultado = new ArrayList<>();
-        for (Projeto projeto : projetos) {
-            if (projeto.getCategoria().equalsIgnoreCase(categoria)) {
-                resultado.add(projeto);
-            }
-        }
+        List<Projeto> resultado = projetos.stream()
+        .filter(projeto -> projeto.getCategoria().equalsIgnoreCase(categoria))
+        .toList();
         return resultado;
     }
 
+    public List<Projeto> buscarPorStatus(String status) {
+        List<Projeto> resultado = projetos.stream()
+        .filter(projeto -> projeto.getStatus().equalsIgnoreCase(status))
+        .toList();
+        return resultado;
+    }
+
+    // remoções
+    public boolean removerPorId(int id) { // não da pra fazer isso um void?
+        for (Projeto projeto : projetos) {
+            if (projeto.getId() == id) {
+                projetos.remove(id);
+                System.out.println("Projeto " + projeto.getId() + " removido com sucesso");
+                return true;
+            }
+        }
+        System.out.println("Projeto não encontrado");
+        return false;
+    }
 }
